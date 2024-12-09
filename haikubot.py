372233ed3a6f5143400f2df1651245e26919fc97
Valeriy Pogorelov <valeriy.pogorelov@together.com>
2024-12-09 17:02:53 +0200

    Add config and message limit

--- haikubot.py
+++ haikubot.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
 from telegram.ext import Application, MessageHandler, filters, CallbackContext
@@ -7,26 +8,30 @@ from telegram import Update
 # Load environment variables from .env file
 load_dotenv()
 
-# Токени беруться з середовища
+# Tokens from the environment
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+MESSEGE_LIMIT_DEFAULT = 20
+
 client = OpenAI()
+
+# Load configuration from config.json
+with open('config.json', 'r', encoding='utf-8') as config_file:
+    config = json.load(config_file)
+
+message_limit = config.get('message_limit', MESSEGE_LIMIT_DEFAULT)
 
 messages_buffer = []
 
 async def handle_message(update: Update, context: CallbackContext):
     if update.message and update.message.text:
         text = update.message.text.strip()
-    else:
-        return
-    
-    if text:
         print(text)
         messages_buffer.append(text)
-        if len(messages_buffer) == 20:
+        if len(messages_buffer) == message_limit:
             prompt = "З цих повідомлень: " + "\n".join(messages_buffer) + "\nЗгенеруй хокку мовою цих повідомлень."
-            completion = completion = client.chat.completions.create(
+            completion = client.chat.completions.create(
                 model="gpt-4o-mini",
                 messages=[{"role": "user", "content": prompt}]
             )
